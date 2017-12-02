56bb3c8258f52ce00cf9145bd5ca824ae83b4897
Sebastian Wetzel <seba.wetzel@gmail.com>
2017-12-02 01:37:15 -0300

    Change variable type

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ let Config = JSON.parse(fs.readFileSync("./config.json"));
 
 if (!Config.isTouchConfigured) {
   Config.configTouchSettings = execSync('./touchConfigurator/xtcal -geometry 1920x1080').toString().trim();
-  let configurator = 'xinput set-prop ' + Config.touchName + " 'Coordinate Transformation Matrix' " + Config.configTouchSettings;
+  const configurator = 'xinput set-prop ' + Config.touchName + " 'Coordinate Transformation Matrix' " + Config.configTouchSettings;
   execSync(configurator);
   Config.isTouchConfigured = true;
   fs.writeFileSync('config.json', JSON.stringify(Config, null, 2));
