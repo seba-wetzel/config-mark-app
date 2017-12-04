03c5229f929b157ebf647c25527413b6de9f260b
Sebastian Wetzel <seba.wetzel@gmail.com>
2017-12-04 11:12:57 -0300

    .

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ var fs = require("fs");
 const {execSync} = require('child_process');
 let Config = JSON.parse(fs.readFileSync("./config.json"));
 
-if (!Config.isTouchConfigured) {
-  Config.configTouchSettings = execSync('./touchConfigurator/xtcal -geometry 1920x1080').toString().trim();
+function configTouch() {
   const configurator = 'xinput set-prop ' + Config.touchName + " 'Coordinate Transformation Matrix' " + Config.configTouchSettings;
   execSync(configurator);
   Config.isTouchConfigured = true;
+}
+
+if (!Config.isTouchConfigured | (Config.configTouchSettings == null)) {
+  Config.configTouchSettings = execSync('./touchConfigurator/xtcal -geometry 1920x1080').toString().trim();
   fs.writeFileSync('config.json', JSON.stringify(Config, null, 2));
 }
 
