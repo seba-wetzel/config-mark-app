eb8e89808785430ba15d1a07e1de410dfb7a3bd4
Sebastian Wetzel <seba.wetzel@gmail.com>
2017-12-04 14:41:35 -0300

    .

--- app.js
+++ app.js
@@ -4,13 +4,35 @@ let Config = JSON.parse(fs.readFileSync("./config.json"));
 
 function configTouch() {
   const configurator = 'xinput set-prop ' + Config.touchName + " 'Coordinate Transformation Matrix' " + Config.configTouchSettings;
-  execSync(configurator);
-  Config.isTouchConfigured = true;
+  try {
+    var exec = execSync(configurator);
+  } catch (err) {
+    console.log("Hubo un problema al configurar el Tactil");
+
+    //Si no puede guardar las configuraciones las sobre escribe con un null
+    //porque a veces malos valores de config emiten un error en xinput
+    Config.configTouchSettings = null;
+    fs.writeFileSync('config.json', JSON.stringify(Config, null, 2));
+    return;
+  }
+  console.log("Tactil configurado");
 }
 
-if (!Config.isTouchConfigured | (Config.configTouchSettings == null)) {
+if (!Config.isTouchConfigured || (Config.configTouchSettings == null)) {
   Config.configTouchSettings = execSync('./touchConfigurator/xtcal -geometry 1920x1080').toString().trim();
+  Config.isTouchConfigured = true;
   fs.writeFileSync('config.json', JSON.stringify(Config, null, 2));
+  console.log("Guardando configuraciones \nTouch Settings= " + Config.configTouchSettings);
 }
 
-console.log(Config.isTouchConfigured);
+configTouch();
+
+if (Config.isAutoStart) {
+  const app = 'node ' + Config.appDir;
+  try {
+    var exec = execSync(app);
+  } catch (err) {
+    console.log("Hubo un problema al iniciar la app" + err);
+  }
+
+}
