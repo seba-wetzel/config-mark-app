a511be6f940ce7fc0eb098165e0c230eb946635f
seba-wetzel <seba.wetzel@gmail.com>
2017-12-05 14:01:42 +0000

    boludeces de la pi

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var fs = require("fs");
 const {execSync} = require('child_process');
 let Config = JSON.parse(fs.readFileSync("./config.json"));
+let error = false;
+
 
 function configTouch() {
   const configurator = 'xinput set-prop ' + Config.touchName + " 'Coordinate Transformation Matrix' " + Config.configTouchSettings;
@@ -8,17 +10,15 @@ function configTouch() {
     var exec = execSync(configurator);
   } catch (err) {
     console.log("Hubo un problema al configurar el Tactil");
-
-    //Si no puede guardar las configuraciones las sobre escribe con un null
-    //porque a veces malos valores de config emiten un error en xinput
-    Config.configTouchSettings = null;
-    fs.writeFileSync('config.json', JSON.stringify(Config, null, 2));
+    error = true; 
+    
     return;
   }
+  error = true;
   console.log("Tactil configurado");
 }
 
-if (!Config.isTouchConfigured || (Config.configTouchSettings == null)) {
+function calibrator(){
   const calibrator = './touchConfigurator/xtcal -geometry '+ Config.screenGeometry;
   console.log(calibrator);
   Config.configTouchSettings = execSync(calibrator).toString().trim();
@@ -27,10 +27,26 @@ if (!Config.isTouchConfigured || (Config.configTouchSettings == null)) {
   console.log("Guardando configuraciones \nTouch Settings= " + Config.configTouchSettings);
 }
 
+
+
+
+if (!Config.isTouchConfigured || (Config.configTouchSettings == null)) {
+    calibrator();
+}
+
 configTouch();
 
+if (error){
+//Si no puede guardar las configuraciones las sobre escribe con un null
+    //porque a veces malos valores de config emiten un error en xinput
+    //Config.configTouchSettings = null;
+    fs.writeFileSync('config.json', JSON.stringify(Config, null, 2));
+    calibrator();
+
+}
+
 if (Config.isAutoStart) {
-  const app = 'node ' + Config.appDir;
+  const app = 'cd ' + Config.appDir + ' && npm start';
   try {
     var exec = execSync(app);
   } catch (err) {
